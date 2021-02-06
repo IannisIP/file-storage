1702580ee53ac99e2141addcbbcbed30fc0920e6
Iannis Paris <parisiannis16@stud.ase.ro>
2021-02-06 18:59:40 +0200

    Add cors

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require("express");
+const cors = require("cors");
 
 const app = express();
+app.use(cors());
 app.use("/images", express.static(__dirname + "/public"));
 
 const port = process.env.PORT || 3001;
