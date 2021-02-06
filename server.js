e1b2bf06b9c9b8e5662ccd0555368fc2f642f23a
Iannis Paris <parisiannis16@stud.ase.ro>
2021-02-06 23:46:07 +0200

    Add page.

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const cors = require("cors");
 const app = express();
 app.use(cors());
 app.use("/images", express.static(__dirname + "/public"));
+app.use("/project", express.static(__dirname + "/public"));
 
 const port = process.env.PORT || 3001;
 
